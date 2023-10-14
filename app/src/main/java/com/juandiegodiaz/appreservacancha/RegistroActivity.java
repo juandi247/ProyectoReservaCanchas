7478f474dfb4e62ab683988e6db208c3eec17a30
juandi247 <jdiaz873@unab.edu.co>
2023-10-14 01:20:35 -0500

    formulario registro ya terminado, conectado con la base de datos de firebase y funcionando!!!

--- app/src/main/java/com/juandiegodiaz/appreservacancha/RegistroActivity.java
+++ app/src/main/java/com/juandiegodiaz/appreservacancha/RegistroActivity.java
@@ -1,6 +1,8 @@
 package com.juandiegodiaz.appreservacancha;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.FirebaseApp;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,26 +11,69 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegistroActivity extends AppCompatActivity {
+
+
+private FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
         Button btnVolver = findViewById(R.id.btn_VolverInicioSesion);
-
+        FirebaseApp.initializeApp(this);
         Button btnRegistro = findViewById(R.id.btn_terminarRegistro);
         EditText usuarioEditText = findViewById(R.id.et_usuarioRegistro);
         EditText contrasenaET = findViewById(R.id.et_contrasenaRegistro);
         EditText nombreEditText = findViewById(R.id.et_nombre);
         EditText apellidoEditText = findViewById(R.id.et_apellido);
 
+        db= FirebaseFirestore.getInstance();
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
 
+                String usuario = usuarioEditText.getText().toString();
+                String contraseña = contrasenaET.getText().toString();
+                String nombre = nombreEditText.getText().toString();
+                String apellido = apellidoEditText.getText().toString();
+
+                if (usuario.isEmpty() || contraseña.isEmpty() || nombre.isEmpty() || apellido.isEmpty() ) {
+
+                    Toast.makeText(RegistroActivity.this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
+                } else {
+                    try {
+                        // Crear un nuevo documento en la colección "usuarios"
+                        Map<String, Object> userData = new HashMap<>();
+                        userData.put("usuario", usuario);
+                        userData.put("contraseña", contraseña);
+                        userData.put("nombre", nombre);
+                        userData.put("apellido", apellido);
+                        userData.put("fecha",0);
+                        userData.put("hora",0);
 
 
-
-
+                        db.collection("Usuarios")
+                                .add(userData)
+                                .addOnSuccessListener(documentReference -> {
+                                    Toast.makeText(RegistroActivity.this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show();
+                                    // Redirigir a otra actividad o realizar alguna otra acción aquí
+                                })
+                                .addOnFailureListener(e -> {
+                                    Toast.makeText(RegistroActivity.this, "Error al registrar el usuario", Toast.LENGTH_SHORT).show();
+                                });
+                    } catch (Exception e) {
+                        Toast.makeText(RegistroActivity.this, "Error inesperado: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
 
         btnVolver.setOnClickListener(new View.OnClickListener() {
@@ -40,22 +85,5 @@ public class RegistroActivity extends AppCompatActivity {
             }
         });
 
-
-        btnRegistro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String usuario = usuarioEditText.getText().toString();
-                String contraseña = contrasenaET.getText().toString();
-                String nombre = nombreEditText.getText().toString();
-                String apellido = apellidoEditText.getText().toString();
-                if (usuario.isEmpty() || contraseña.isEmpty() || nombre.isEmpty() || apellido.isEmpty() ) {
-                    // Al menos uno de los campos está en blanco, muestra una alerta
-                    Toast.makeText(RegistroActivity.this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
-                } else {
-                    // Ambos campos tienen valores, procede con el inicio de sesión
-                    // Agrega aquí el código para iniciar sesión
-                }
-            }
-        });
     }
 }
