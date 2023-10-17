7711c136c5cc8c0147eabb3cdc6e91a17564b9dc
StebanG4 <egarcia93@unab.edu.co>
2023-10-17 15:22:41 +0200

    creacion del activity de calendario para el boton de cancelar y ver reserva

---
+++ app/src/main/java/com/juandiegodiaz/appreservacancha/CalendarioActivity.java
@@ -0,0 +1,14 @@
+package com.juandiegodiaz.appreservacancha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalendarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendario);
+    }
+}
