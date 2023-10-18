4f32bd977a8d8624bcd6bff19b721f430c5358c4
juandi247 <jdiaz873@unab.edu.co>
2023-10-18 10:01:41 -0500

    Cancelar boton Funcionando

--- app/src/main/java/com/juandiegodiaz/appreservacancha/CalendarioActivity.java
+++ app/src/main/java/com/juandiegodiaz/appreservacancha/CalendarioActivity.java
@@ -1,8 +1,10 @@
 package com.juandiegodiaz.appreservacancha;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,9 +13,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CalendarioActivity extends AppCompatActivity {
     private FirebaseFirestore db;
@@ -28,7 +36,7 @@ public class CalendarioActivity extends AppCompatActivity {
         Button btncancelarReserva = findViewById(R.id.btn_cancelarReserva);
         SharedPreferences sharedPreferences = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
         String usuario = sharedPreferences.getString("usuario", "UsuarioPredeterminado");
-        String hora=sharedPreferences.getString("hora reserva","horaPre");
+
         DocumentReference userDocRef = db.collection("Usuarios").document(usuario);
 
 
@@ -40,12 +48,84 @@ public class CalendarioActivity extends AppCompatActivity {
                 .addOnSuccessListener(documentSnapshot -> {
                     if (documentSnapshot.exists()) {
                         Boolean reservaActiva = documentSnapshot.getBoolean("reserva activa");
-                        if (reservaActiva != null && !reservaActiva) {
+                        if (reservaActiva != null && reservaActiva) {
                             // El usuario tiene una reserva activa, muestra el mensaje
-                            btncancelarReserva.setVisibility(View.GONE);
+                            btncancelarReserva.setVisibility(View.VISIBLE);
+
+                            String horaReserva = documentSnapshot.getString("hora reserva");
+                            String fechaReserva = documentSnapshot.getString("fecha reserva");
+                            String canchaReservada = documentSnapshot.getString("nombre cancha reservada");
+
+
+                            Log.d("DatosUsuario", "Hora de Reserva: " + horaReserva);
+                            Log.d("DatosUsuario", "Fecha de Reserva: " + fechaReserva);
+                            Log.d("DatosUsuario", "Cancha Reservada: " + canchaReservada);
+
+                            btncancelarReserva.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+
+                                    new AlertDialog.Builder(CalendarioActivity.this)
+                                            .setTitle("Confirmar Cancelacion")
+
+                                   .setMessage("¿Seguro que deseas cancelar tu reserva?")
+
+                                  .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            //BASE DE DATOS!
+                                            SharedPreferences sharedPreferences = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+                                            String usuario = sharedPreferences.getString("usuario", "UsuarioPredeterminado");
+                                            DocumentReference userDocRef = db.collection("Usuarios").document(usuario);
+
+
+
+
+                                            userDocRef.get()
+                                                    .addOnSuccessListener(documentSnapshot -> {
+                                                        if (documentSnapshot.exists()) {
+                                                            String fechaReserva = documentSnapshot.getString("fecha reserva");
+                                                            String horaReserva = documentSnapshot.getString("hora reserva");
+                                                            String canchaReservada = documentSnapshot.getString("nombre cancha reservada");
+                                                            canchaReservada="Cancha "+canchaReservada;
+                                                            // Actualiza el horario correspondiente en la cancha
+                                                            DocumentReference canchaDocRef = db.collection("Canchas").document(canchaReservada).collection("Fechas").document(fechaReserva);
+                                                            Map<String, Object> horarioUpdate = new HashMap<>();
+                                                            horarioUpdate.put(horaReserva, "Disponible");
+
+                                                            canchaDocRef.set(horarioUpdate, SetOptions.merge());
+
+                                                            // Elimina los campos relacionados con la reserva en el documento del usuario
+                                                            Map<String, Object> reservaData = new HashMap<>();
+                                                            reservaData.put("hora reserva", "");
+                                                            reservaData.put("fecha reserva", "");
+                                                            reservaData.put("nombre cancha reservada", "");
+                                                            reservaData.put("reserva activa", false);
+
+                                                            userDocRef.update(reservaData)
+                                                                    .addOnSuccessListener(aVoid -> {
+                                                                        Toast.makeText(CalendarioActivity.this, "Reserva cancelada con éxito", Toast.LENGTH_SHORT).show();
+                                                                        Intent intent = new Intent(CalendarioActivity.this, InicioActivity.class);
+                                                                        startActivity(intent);
+                                                                    });
+                                                        }
+
+                                            dialog.dismiss();
+                                        });}
+                                    })
+                                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                        }
+                                    })
+                                   .show();
+                                }
+                            });
+
                         } else {
                             // No hay reserva activa, oculta el boton
-                            btncancelarReserva.setVisibility(View.VISIBLE);
+                            btncancelarReserva.setVisibility(View.GONE);
+
+
                         }
                     }
                 })
@@ -69,3 +149,11 @@ public class CalendarioActivity extends AppCompatActivity {
 
     }
 }
+
+
+
+
+
+
+
+
