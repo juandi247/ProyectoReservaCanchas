533a90451d6aceb127932dde6cc692aa41a6bd50
Esteban <egarcia93@unab.edu.co>
2023-10-18 10:03:19 -0500

    Agregar Boton cerrar sesion

--- app/src/main/java/com/juandiegodiaz/appreservacancha/PerfilActivity.java
+++ app/src/main/java/com/juandiegodiaz/appreservacancha/PerfilActivity.java
@@ -2,15 +2,31 @@ package com.juandiegodiaz.appreservacancha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class PerfilActivity extends AppCompatActivity {
 
-    @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
-        Log.d("coso", "hola: ");
+
+        Button btnGoToInicio = findViewById(R.id.cambiar_contraseña_btn);
+
+        btnGoToInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar la actividad de inicio
+                Intent intent = new Intent(PerfilActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
 }
